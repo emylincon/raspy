9f66187a1f8e9071fc15c7baa446ba8597b04c77
Emeka Ugwuanyi <emylincon@gmail.com>
2019-06-25 20:49:13 +0100

    install python3.6 on raspberry pi

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apt-get install python3-dev libffi-dev libssl-dev -y
+wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+tar xJf Python-3.6.3.tar.xz
+cd Python-3.6.3
+./configure
+make
+make install
+pip3 install --upgrade pip
