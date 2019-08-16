d30ca23ef32afd5523a921410712d269b142b582
Emeka Ugwuanyi <emylincon@gmail.com>
2019-08-16 20:31:58 +0100

    improved. . .

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ clear
 echo "============== Preparing to Install Python3.6 ==================="
 sleep 3
 apt update && apt upgrade
-
+apt install make -y
 apt-get install python3-dev libffi-dev libssl-dev -y
 wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
 apt-get install xz-utils
 tar -xf Python-3.6.3.tar.xz
 cd Python-3.6.3
-apt install gcc g++
+apt install gcc g++ -y
 ./configure
 make
 make install
